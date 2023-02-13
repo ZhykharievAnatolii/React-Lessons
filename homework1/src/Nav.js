0ced62ceeb107e372961fe367089b407de723619
ZhykharievAnatolii <nekijt87@gmail.com>
2023-02-13 20:47:36 +0200

    upd nav

--- homework1/src/Nav.js
+++ homework1/src/Nav.js
@@ -4,13 +4,14 @@ export const Nav=()=>{
         <div className="">
         <ul className="nav flex-column">
             <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Active</a>
+                <p className="card-text">How to reach me</p>
+                <a href="https://github.com/ZhykharievAnatolii" className="btn btn-primary m-md-4 bg-dark">Link to my GitHub page</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a href="mailto:nekijt87@gmail.com" className="btn btn-primary m-md-4 bg-dark">email me</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a href="tel:+30634726870" className="btn btn-primary m-md-4 bg-dark">call me or send a message</a>
             </li>
             <li className="nav-item">
                 <a className="nav-link disabled">Disabled</a>
